ab1335e9cc989f1818fa2dae6f3f53a4d2bdd57a
jas <jaslin00@gmail.com>
2020-06-22 23:38:25 +0800

    support sub domain for github

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,25 @@ chmod 600 /root/.ssh/id_rsa
 ssh-keyscan -t rsa github.com >> /root/.ssh/known_hosts
 ssh-keyscan -t rsa e.coding.net >> /root/.ssh/known_hosts
 
+cat << EOF > /root/.ssh/config
+Host github.com  
+    HostName github.com  
+    PreferredAuthentications publickey  
+    IdentityFile /root/.ssh/id_rsa
+
+Host $GITHUB_ACTOR.github.com  
+    HostName github.com  
+    PreferredAuthentications publickey  
+    IdentityFile /root/.ssh/id_rsa
+
+Host e.coding.net  
+    HostName e.coding.net  
+    PreferredAuthentications publickey  
+    IdentityFile /root/.ssh/coding_rsa 
+EOF
+
+chmod 600 /root/.ssh/config
+
 # setup deploy git account
 git config --global user.name "$INPUT_USER_NAME"
 git config --global user.email "$INPUT_USER_EMAIL"
