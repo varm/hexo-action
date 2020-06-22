d675c2d30dd9c17ed43d4add56b31b2b8556dd2b
jas <jaslin00@gmail.com>
2020-06-22 22:54:20 +0800

    do not support different deploy key beetween github and coding

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,27 +5,11 @@ set -e
 # setup ssh-private-key
 mkdir -p /root/.ssh/
 echo "$INPUT_DEPLOY_KEY" > /root/.ssh/id_rsa
-echo "$INPUT_DEPLOY_KEY_CODING" > /root/.ssh/coding_rsa
 
 chmod 600 /root/.ssh/id_rsa
-chmod 600 /root/.ssh/coding_rsa
 
 ssh-keyscan -t rsa github.com >> /root/.ssh/known_hosts
 ssh-keyscan -t rsa e.coding.net >> /root/.ssh/known_hosts
-
-cat << EOF > /root/.ssh/config
-Host github.com  
-    HostName github.com  
-    PreferredAuthentications publickey  
-    IdentityFile /root/.ssh/id_rsa
-
-Host e.coding.net  
-    HostName e.coding.net  
-    PreferredAuthentications publickey  
-    IdentityFile /root/.ssh/coding_rsa 
-EOF
-
-chmod 600 /root/.ssh/config
 
 # setup deploy git account
 git config --global user.name "$INPUT_USER_NAME"
